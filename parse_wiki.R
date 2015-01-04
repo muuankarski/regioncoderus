4ce80a189c276976cab101bb3a744a9b51b29221
Markus Kainu <markuskainu@gmail.com>
2015-01-04 17:05:07 +0200

    trim_region_names_rus trims now into wikipedia scheme. Most changes relate to that
    Also, federal districts and economiv regions and Russia added as convertible region names

--- parse_wiki.R
+++ parse_wiki.R
@@ -3,8 +3,10 @@ library(RCurl)
 library(XML)
 library(stringr)
 
-# 1. Lets parse the table of  all sovereign states from here: http://en.wikipedia.org/wiki/List_of_sovereign_states
+# 1. Lets parse the table of  all sovereign states from here: http://en.wikipedia.org/wiki/Autonomous_okrugs_of_Russia
 # to get url's to English page of each state
+
+##
 
 html <- getURL("http://en.wikipedia.org/wiki/Autonomous_okrugs_of_Russia", followlocation = TRUE)
 
@@ -12,25 +14,82 @@ html <- getURL("http://en.wikipedia.org/wiki/Autonomous_okrugs_of_Russia", follo
 doc <-  htmlParse(html, asText=TRUE)
 tbls <- xpathSApply(doc, "//table[@class='nowraplinks navbox-subgroup']", saveXML)
 x <- tbls[nchar(tbls) == max(nchar(tbls))]
-str <- unlist(strsplit(x, '<li><a href=\"/wiki/')) 
-str <- gsub(pattern = '\\\"(.*)', replacement = '', x = str, perl = TRUE)
+str <- unlist(strsplit(x, '<li><a href=\"/wiki/'))
 str <- gsub(pattern = '\\\"(.*)', replacement = '', x = str, perl = TRUE)
 str <- gsub(pattern = '(\\\n)(.*)', replacement = "", x = str, perl = TRUE)
 
 # one custom fix
 str[str == "North_Ossetia%E2%80%93Alania"] <- "North_Ossetia–Alania"
 
-regions <- str[-1]
-# iadd ones not included in the list, but can be found in countrycode-package
-#countries <- c(countries,"Aland_Islands")
+regions1 <- str[-1]
+
+## ---------------------------------------------------------- ##
+
+## Federal districts from http://en.wikipedia.org/wiki/Federal_districts_of_Russia
+
+html <- getURL("http://en.wikipedia.org/wiki/Federal_districts_of_Russia", followlocation = TRUE)
+
+# parse html
+doc <-  htmlParse(html, asText=TRUE)
+tbls <- xpathSApply(doc, "//table[@class='wikitable sortable']", saveXML)
+x <- tbls
+str <- unlist(strsplit(x, '<a href=\"/wiki/'))
+str <- gsub(pattern = '\\\"(.*)', replacement = '', x = str, perl = TRUE)
+str <- gsub(pattern = '(\\\n)(.*)', replacement = "", x = str, perl = TRUE)
+# only including "federal_district"
+
+regions2 <- str[grep("Federal_District", str)]
+
+## ---------------------------------------------------------- ##
+
+## Economical regions from http://en.wikipedia.org/wiki/Economic_regions_of_Russia
+
+html <- getURL("http://en.wikipedia.org/wiki/Economic_regions_of_Russia", followlocation = TRUE)
+
+# parse html
+doc <-  htmlParse(html, asText=TRUE)
+tbls <- xpathSApply(doc, "//div[@class='legend']", saveXML)
+str <- tbls
+str <- gsub(pattern = '(.*)<a href=\"/wiki/', replacement = '', x = str, perl = TRUE)
+str <- gsub(pattern = '\\\"(.*)', replacement = '', x = str, perl = TRUE)
+regions3 <- str
+
+
+## Autonomous okrugs from : http://en.wikipedia.org/wiki/Autonomous_okrugs_of_Russia
+
+html <- getURL("http://en.wikipedia.org/wiki/Autonomous_okrugs_of_Russia", followlocation = TRUE)
+
+# parse html
+doc <-  htmlParse(html, asText=TRUE)
+tbls <- xpathSApply(doc, "//table[@class='wikitable sortable']", saveXML)
+x <- tbls[2]
+str <- unlist(strsplit(x, '<a href=\"/wiki/'))
+str <- gsub(pattern = '\\\"(.*)', replacement = '', x = str, perl = TRUE)
+str <- gsub(pattern = '(\\\n)(.*)', replacement = "", x = str, perl = TRUE)
+
+# one custom fix
+str[str == "Khanty%E2%80%93Mansi_Autonomous_Okrug"] <- "Khanty-Mansi_Autonomous_Okrug"
+
+regions4 <- str[-1]
+
+
+## ---------------------------------------------------------- ##
+
+## Russian Federation
+
+regions5 <- "Russia"
+
+regions <- c(regions1,regions2,regions3,regions4,regions5)
+# remove duplicated url's  (Some scemes are not exclusive)
+regions <- regions[!duplicated(regions)]
+
+# custom fix- remove parenthesis from Northern_economic_region_(Russia) 
+regions[regions == "Northern_economic_region_(Russia)"] <- "Northern_economic_region,_Russia"
+regions[regions == "Urals_economic_region"] <- "Ural_economic_region"
+regions <- regions[regions != "Kaliningrad_economic_region"]  # Manually adding this on row ~116
 
 urls <- paste0("http://en.wikipedia.org/wiki/",regions)
 
-
-
-# 2. Now we have the links, so lets go through each country page and 
-# fetch the links to each language version of that page - and get there the
-# language name in English AND country name in the language in question
 
 for (i in 1:length(urls)){
   
@@ -51,17 +110,20 @@ for (i in 1:length(urls)){
   str <- str_trim(str)
   str <- str_replace_all(str, "'","")
   
-  
   ## as there are some strings with no ; marks, we get rid of them
   dd <- read.table(text = str, sep = ";", colClasses = "character")
   names(dd) <- c(regions[i],"lang")
+  if (i == 99) names(dd)[1] <- str_replace_all(names(dd)[1], ',_Russia','') 
   ## English language region name from title-element
   title <- xpathSApply(doc, "//title", saveXML)
   title <- gsub(pattern = "( - Wikipedia)(.*)", replacement = "", x = title, perl = TRUE)
   title <- str_replace_all(title, '<title>','')
+  # fix this exception ""Northern economic region, Russia"
+  if (i == 99) title <- str_replace_all(title, ', Russia','')
+  if (i == 108) title <- str_replace_all(title, 'Koryak Okrug','Koryak Autonomous Okrug')
   newrow <- data.frame(title,"English")
-  title <- str_replace_all(title, " ", "_")
-  names(newrow) <- c(title,"lang")
+  titlename <- str_replace_all(title, " ", "_")
+  names(newrow) <- c(titlename,"lang")
   dd <- rbind(dd,newrow)
   #
   if (i == 1) dat <- dd
@@ -69,9 +131,22 @@ for (i in 1:length(urls)){
   if (i != 1) dat <- merge(dat,dd,by="lang", all.x=TRUE)
 }
 
-# finally, transpose the data
+kaliningrad_economic_region <- c(rep(NA, 22),
+                                 "Kaliningrad Economic Region",
+                                 rep(NA, 3),
+                                 "Kaliningradin talousalue",
+                                 rep(NA, 40),
+                                 "Калининградский экономический район",
+                                 rep(NA, 27))
+dat <- cbind(dat,kaliningrad_economic_region)
 
+# finally, transpose the data
 data <- as.data.frame(t(dat[-1]))
+
+# manually add Kaliningrad economic regions
+
+
+
 
 # create language names from row.names of the dat and refine them a bit
 new_names <- paste0("region.name.",tolower(as.character(dat[[1]])))
